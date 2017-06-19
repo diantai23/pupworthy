d9059d738ed29438e09cce0c858cdaa03533314b
Dian Tai <diantai@gmail.com>
2017-06-19 23:55:08 +0000

    Lesson 18: Support create comments on grams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_error(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 end
